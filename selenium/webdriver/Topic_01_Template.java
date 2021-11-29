bf47957eeece4cea7c8335878f80c9a69f99f688
Tran Van Cuong <cuongtr245@gmail.com>
2021-11-29 20:39:23 +0700

    Updated Topic 2_ Located

--- selenium/webdriver/Topic_01_Template.java
+++ selenium/webdriver/Topic_01_Template.java
@@ -10,7 +10,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_01_Check_Environment {
+public class Topic_01_Template {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
 
@@ -24,27 +24,23 @@ public class Topic_01_Check_Environment {
 	}
 
 	@Test
-	public void TC_01_ValidateCurrentUrl() {
-		// Login Page Url matching
-		String loginPageUrl = driver.getCurrentUrl();
-		Assert.assertEquals(loginPageUrl, "https://www.facebook.com/");
-	}
+	public void TC_01() {
 
+	}
 	@Test
-	public void TC_02_ValidatePageTitle() {
-		// Login Page title
-		String loginPageTitle = driver.getTitle();
-		Assert.assertEquals(loginPageTitle, "Facebook – log in or sign up");
-	}
+	public void TC_02() {
 
-	@Test
-	public void TC_03_LoginFormDisplayed() {
-		// Login 
-		Assert.assertTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
 	}
-
-//	@AfterClass
-//	public void afterClass() {
-//		driver.quit();
-//	}
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+	public void sleepInSecond (long sencond) {
+		try {Thread.sleep(sencond * 1000);
+		
+		}catch(InterruptedException e){
+			e.printStackTrace();
+		}
+			
+	}
 }
